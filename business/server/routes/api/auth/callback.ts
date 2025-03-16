45483ea0faaf1c6070ec4a68991123f7aad5b738
Kent Ntwari <designerjehovah@gmail.com>
2025-03-16 11:47:12 -0400

    refactor: change response type

--- business/server/routes/api/auth/callback.ts
+++ business/server/routes/api/auth/callback.ts
@@ -5,7 +5,7 @@ export default defineEventHandler(async (event) => {
   try {
     const url = getRequestURL(event);
     await kindeClient.handleRedirectToApp(event.context.session, url);
-    return sendRedirect(event, "/");
+    return sendRedirect(event, url.toString());
   } catch (error) {
     throw new CallbackError();
   }

da35d7f45bfdf990f07756effa96aa9440c99680
Kent Ntwari <designerjehovah@gmail.com>
2025-05-18 08:40:14 -0400

    feat: listen to clerk webhook events

---
+++ www/app/routes/_ressource.webhook.clerk.ts
@@ -0,0 +1,11 @@
+import type { Route } from "./+types/_ressource.webhook.clerk";
+
+import { ClerkWebhookController } from "controllers/webhook.clerk";
+
+export async function loader({ params, context, request }: Route.LoaderArgs) {
+  return {};
+}
+
+export async function action({ request }: Route.ActionArgs) {
+  return new ClerkWebhookController(request).handleWebhookEvent();
+}

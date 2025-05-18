3a0d9ff7c56be55ed6f810d0c1ea263845212dbf
Kent Ntwari <designerjehovah@gmail.com>
2025-05-18 08:18:19 -0400

    init app utils

---
+++ business/utils/schemas.zod.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+export type TUserSchema = z.infer<typeof userSchema>;
+export type TUserCreateSchema = z.infer<typeof userCreateSchema>;
+
+export const apiResponseSchema = z.object({
+  success: z.boolean(),
+  error: z.string().nullable(),
+  data: z.object({}).nullable(),
+});
+
+export const userSchema = z.object({
+  id: z.string().min(1),
+  email: z.string().email(),
+  firstName: z.string().min(1),
+  lastName: z.string().min(1),
+  imageUrl: z.string().url().or(z.null()),
+});
+
+export const userCreateSchema = userSchema;
+
+export const verifyUnkeyResponseSchema = z.object({
+  valid: z.boolean(),
+  keyId: z.string(),
+  permissions: z.array(z.string()),
+  roles: z.array(z.string()),
+});
+
+export const verifyUnkeyApiClientResponseSchema = z.object({
+  success: z.boolean(),
+  error: z.string().nullable(),
+  data: z.object({}).nullable(),
+});
---
+++ www/utils/schemas.zod.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+
+export const UnkeyCreateKeyRequestOptionsSchema = z.object({
+  name: z.string().min(1).startsWith("guestify_"),
+  apiId: z.string().includes("api"),
+  externalId: z.string().min(1),
+  roles: z.array(z.string()).optional(),
+  permissions: z.array(z.string()).optional(),
+  meta: z.object({}).optional(),
+  byteLength: z.number().min(16).optional(),
+});
+
+export const UnkeyCreateKeyResponseSchema = z.object({
+  key: z.string().min(1),
+  keyId: z.string().min(1).includes("key"),
+});
+
+export const UnkeyErrorResponseSchema = z.object({
+  error: z.object({
+    code: z.string(),
+    docs: z.string().url(),
+    message: z.string(),
+    requestId: z.string(),
+  }),
+});

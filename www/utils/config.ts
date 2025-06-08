4a990ca222fdc4944d8b9f7bf9ad146b207d8f62
Kent Ntwari <designerjehovah@gmail.com>
2025-06-08 06:56:30 -0400

    feat: add default options for updating clerk user metadata

--- www/utils/config.ts
+++ www/utils/config.ts
@@ -1,8 +1,9 @@
 import type { FetchOptions } from "ofetch";
+import type { User } from "@clerk/react-router/ssr.server";
 import type { UserEntity } from "entities/user";
+import type { TUnkeyCreateKeyRequestOptions } from "./schemas.zod";
 
 import { WebhookClerkDTO } from "dto/webhook.clerk";
-import type { TUnkeyCreateKeyRequestOptions } from "./schemas.zod";
 
 type BaseHeaders = {
   Authorization: `Bearer ${string}`;
@@ -34,6 +35,17 @@ export class ConfigUtils {
       ...opts,
       method: "POST",
       query: WebhookClerkDTO.createNewUserQuery(user),
+    };
+  }
+
+  static updateClerkUserMetadataOpts(
+    user: ReturnType<(typeof WebhookClerkDTO)["updateUserMetadata"]>,
+    opts: IOpts
+  ) {
+    return {
+      ...opts,
+      method: "PATCH",
+      body: { ...user },
     };
   }
 }

c9913b5a441a7661dc6ef73a9978fa67c946ee10
Kent Ntwari <designerjehovah@gmail.com>
2025-11-10 17:30:35 -0500

    refactor: centralized key verification in hook

--- business/server/plugins/verify.unkey.ts
+++ business/server/plugins/verify.unkey.ts
@@ -1,5 +1,5 @@
-import { UnkeyController } from "@/controllers/unkey";
-import { UnkeyFactory } from "@/factory/unkey";
+import { UnkeyService } from "@/_services/unkey";
+import { NetworkError } from "@/errors/network";
 
 declare module "h3" {
   interface H3EventContext {
@@ -9,10 +9,19 @@ declare module "h3" {
 
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook("request", async (event) => {
-    const res = await new UnkeyController(event).verify(
-      UnkeyFactory.parseAuthorizationHeader(getRequestHeaders(event))["key"] ??
-        ""
-    );
+    const authHeader =
+      getRequestHeaders(event)["authorization"] ||
+      getRequestHeaders(event)["Authorization"];
+
+    if (!authHeader)
+      throw new NetworkError("Authorization header is missing", 401);
+
+    const [scheme, userKey] = authHeader.split(" ");
+
+    if (scheme !== "Bearer")
+      throw new NetworkError("Invalid authorization format", 400);
+
+    const res = await new UnkeyService().verifyUnkeyUserKey(userKey);
 
     if (!res.success) event.context.isUserVerified = false;
     else event.context.isUserVerified = true;
